1c73f5b0a8b06cbba197b668a2b34ada69b36530
Jeni Williams <stanfordjeni@gmail.com>
2024-12-03 20:21:14 -0800

    Initial commit of To Do List app project files

---
+++ todolist_app.py
@@ -0,0 +1,39 @@
+### PROJECT REQUIREMENTS ###
+# ** User Interface (UI) and Storage Method **
+# - Build a simple Command Line Interface (CLI) that welcomes users 
+# - Displays a menu with options to add, view, delete tasks, quit the application.
+# => The tasks should be stored in a Python list
+
+# ** Core Features **
+# - Add tasks
+# - View tasks
+# - Delete tasks
+# - Quit the application
+
+# ** User Interaction **
+# - Use input() to capture user selections and ensure proper input 
+# validation to handle invalid choices.
+
+# ** Error Handling **
+# - Implement error handling using try, except, else, and finally blocks to catch errors
+# - Alert the user if they provide invalid input
+# - Alert the user if there are no tasks to view
+# - Alert the user if they try to delete a task that doesn't exist
+# - Alert the user if they select an option on the main menu that doesn't exist
+
+# ** Code Organization **
+# - Organize your code into functions to improve clarity and maintainability. 
+# - Use descriptive function names and add comments/docstrings where necessary.
+
+# ** Testing and Debugging
+# - Thoroughly test your application, considering edge cases
+# such as empty lists and invalid input.
+
+### PROJECT CODE ###
+# Global Variables
+
+# Functions List
+
+# Project Code
+
+# ** FINAL STEP: Create a README.md on the repository that gives information about your project and how to run/use it
